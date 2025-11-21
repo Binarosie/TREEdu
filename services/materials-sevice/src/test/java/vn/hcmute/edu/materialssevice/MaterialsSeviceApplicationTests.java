68f7fbf52155b731a61e6ca018ff6e955de0752b
Trần Như  Quỳnh <trankienquynh.070820@gmail.com>
2025-11-21 11:23:58 +0700

    base

---
+++ services/materials-sevice/src/test/java/vn/hcmute/edu/materialssevice/MaterialsSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hcmute.edu.materialssevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialsSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
